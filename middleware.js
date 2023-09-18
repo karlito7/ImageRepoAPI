a345a7d8069093316bcad893fa94191e1f1042ea
Karlo P <kpericic@student.unipu.hr>
2023-09-19 00:45:45 +0200

    Fix middleware functions

--- middleware.js
+++ middleware.js
@@ -4,11 +4,11 @@ const imageQuery = require('./imageQuery');
 
 /**         USER OPERATIONS         */
 const createUser = async (request, response) => {
-    const {username, password, image} = request.body;
+    const {username, password} = request.body;
 
     try {
-        const user = await userQuery.create(username, password, image);
-        response.status(200);
+        const user = await userQuery.create(username, password);
+        response.status(200).json(user);
     } catch (err) {
         response.status(400).json(err);
     }
@@ -29,6 +29,20 @@ const login = async (request, response) => {
 
 /**         IMAGE OPERATIONS         */
 const uploadImage = async (request, response) => {
+    const {originalname, mimetype, path, size} = request.file;
+    const {id} = request.params;
+    
+    try {
+        const resList = await imageQuery.upload(originalname, mimetype, path, size, id);
+        console.log("RES >> ", resList);
+        response.status(200).json(resList);
+    } catch (err) {
+        response.status(400).json(err);
+    }
+};
+
+const uploadImages = async (request, response) => {
+    console.log("request.body >> ", request.file);
     const {list} = request.body;
 
     try {
@@ -69,6 +83,7 @@ module.exports = {
     createUser,
     login,
     uploadImage,
+    uploadImages,
     readImageByUserId,
     deleteImageById
 }
